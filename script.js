853141cb32a1116110d76a073b0a61e37c638afe
scanner-smartcontract <hh3437093@gmail.com>
2025-01-31 23:31:33 +0700

    Update website

---
+++ script.js
@@ -0,0 +1,5 @@
+function analyzeToken() {
+    const contractAddress = document.getElementById('contractAddress').value;
+    // Contoh sederhana: Tampilkan alamat yang dimasukkan
+    document.getElementById('result').innerText = `Analyzing contract: ${contractAddress}`;
+}
